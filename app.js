106ae5bfe0723bfba5ce275042a2fec921519e9e
Aditya <adijr9487@gmail.com>
2022-10-02 22:45:10 +0530

    added distribution picker

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 import { Picker } from "pick-random-weighted";
 
+import pick from 'pick-random-weighted';
+
+const colors = [
+	['Red', 30],
+	['Green', 20],
+	['Blue', 40]
+];
+
+const color = pick(colors);
